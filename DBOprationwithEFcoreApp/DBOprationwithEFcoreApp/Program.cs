7a4e0032f64b5d44ef03354acbcf5612709d3a6f
Bharti Thombare <bhartithombare146@gmail.com>
2026-03-12 12:42:12 +0530

    Added coonection string

--- DBOprationwithEFcoreApp/DBOprationwithEFcoreApp/Program.cs
+++ DBOprationwithEFcoreApp/DBOprationwithEFcoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DBOprationwithEFcoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DBOprationwithEFcoreApp
 {
     public class Program
@@ -6,6 +9,8 @@ namespace DBOprationwithEFcoreApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
 
             // Add services to the container.
 
